c04cd5365a2e56b697dc3d6cc0a63507185f6d3d
Yves-X <Yves-X@foxmail.com>
2018-03-28 16:14:59 +0800

    update meta infomation

--- ithomeCpsFliter.user.js
+++ ithomeCpsFliter.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name                ithome cps filter
+// @name                IT之家资讯列表广告屏蔽 ithome CPS Filter
 // @name:zh-CN          IT之家资讯列表广告屏蔽
-// @description         Remove CPS Ads in ithome.com's news list.
-// @description:zh-CN   Remove CPS Ads in ithome.com's news list.
+// @description         移除IT之家（ithome.com）资讯列表内投放的购物广告；Remove CPS Ads in ithome.com's news list.
+// @description:zh-CN   移除IT之家（ithome.com）资讯列表内投放的购物广告。
 // @version             1.0
 // @author              YvesX https://www.yvesx.com
 // @namespace           https://github.com/Yves-X
@@ -13,11 +13,11 @@
 // ==/UserScript==
 (function () {
     'use strict';
-        var newsSpan = $('.title').children('a');
-        newsSpan.each(function (i) {
-            var newsUrl = $(this).attr("href");
-            if (newsUrl.indexOf('lapin') >= 0) {
-                $(this).parent().parent().html('&nbsp;');
-            }
-        });
+    var newsSpan = $('.title').children('a');
+    newsSpan.each(function (i) {
+        var newsUrl = $(this).attr("href");
+        if (newsUrl.indexOf('lapin') >= 0) {
+            $(this).parent().parent().html('&nbsp;');
+        }
+    });
 })();
