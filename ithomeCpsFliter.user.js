f69ac78031b06121ba388f9f4ec2d6b708c44a7d
Yves-X <Yves-X@foxmail.com>
2018-03-28 20:40:38 +0800

    update 1.1 : 适配更多页面
    1. 加入了多页面支持，包括主页、新闻列表以及尚不完整的博客版、WAP视图
    2. 修复了一处错误

--- ithomeCpsFliter.user.js
+++ ithomeCpsFliter.user.js
@@ -3,21 +3,62 @@
 // @name:zh-CN          IT之家资讯列表广告屏蔽
 // @description         移除IT之家（ithome.com）资讯列表内投放的购物广告；Remove CPS Ads in ithome.com's news list.
 // @description:zh-CN   移除IT之家（ithome.com）资讯列表内投放的购物广告。
-// @version             1.0
+// @version             1.1
 // @author              YvesX https://www.yvesx.com
 // @namespace           https://github.com/Yves-X
 // @contributionURL     https://www.yvesx.com/go/donate/
 // @supportURL          https://github.com/Yves-X/ithome-CPS-Filter/issues
-// @match               *://www.ithome.com/*
+// @match               https://*.ithome.com/*
 // @grant               none
 // ==/UserScript==
+
 (function () {
+
     'use strict';
-    var newsSpan = $('.title').children('a');
-    newsSpan.each(function (i) {
-        var newsUrl = $(this).attr("href");
-        if (newsUrl.indexOf('lapin') >= 0) {
-            $(this).parent().parent().html('&nbsp;');
-        }
-    });
+
+    var url = window.location.href;
+    if (url == 'https://www.ithome.com/') {
+        forHomepage();
+    }
+    else if (url.indexOf('www.ithome.com/list/') >= 0) {
+        forList();
+    }
+    else if (url.indexOf('www.ithome.com/blog/') >= 0) {
+        forBlog();
+    }
+    else if (url.indexOf('wap.ithome.com') >= 0) {
+        forWap();
+    }
+
+    function forHomepage() {
+        var newsSpan = $('.title a');
+        newsSpan.each(function (i) {
+            var newsUrl = $(this).attr("href");
+            if (newsUrl.indexOf('lapin') >= 0) {
+                $(this).parent().parent().html('&nbsp;');
+            }
+        });
+    }
+    function forList() {
+        var newsSpan = $('.ulcl a');
+        newsSpan.each(function (i) {
+            var newsUrl = $(this).attr("href");
+            if (newsUrl.indexOf('lapin') >= 0) {
+                $(this).parent().remove();
+            }
+        });
+    }
+    function forBlog() {
+        forList();
+    }
+    function forWap() {
+        var newsSpan = $('.jsonplist a');
+        newsSpan.each(function (i) {
+            var newsUrl = $(this).attr("href");
+            if (newsUrl.indexOf('lapin') >= 0) {
+                $(this).parent().remove();
+            }
+        });
+    }
+
 })();
